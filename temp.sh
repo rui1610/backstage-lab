3f8c4ec332a8ca4e4abea22a185eee9dd67e7f86
Rui Nogueira <rui.nogueira@sap.com>
2024-04-30 14:54:45 +0200

    update setup

--- temp.sh
+++ temp.sh
@@ -4,15 +4,6 @@ source config/secrets/postgres.env
 source config/app.env
 set +o allexport
 
-# Setup environment variables
-# ---------------------------------------------------------------
-# ---------------------------------------------------------------
-
-npx --yes @backstage/create-app@latest 
-
-# Checkout https://backstage.io/docs/getting-started/ for starting the app by changing to the app directory and running the following command
-
-# Change "btprui" to the name of the app you want to create (entered in the previous step)
 yq -i -y ".app.title = \"${APP_TITLE}\""  $APP_NAME/app-config.yaml
 yq -i -y ".organization.name = \"${ORGANIZATION_NAME}\""  $APP_NAME/app-config.yaml
 
@@ -27,7 +18,3 @@ yq -i -y '.backend.database.connection.host     = "${POSTGRES_HOST}"'     $APP_N
 yq -i -y '.backend.database.connection.port     = "${POSTGRES_PORT}"'     $APP_NAME/app-config.yaml
 yq -i -y '.backend.database.connection.user     = "${POSTGRES_USER}"'     $APP_NAME/app-config.yaml
 yq -i -y '.backend.database.connection.password = "${POSTGRES_PASSWORD}"' $APP_NAME/app-config.yaml
-
-cd $APP_NAME
-
-yarn dev
